c7edc9fab80cf53c1fa85e03fafcd20ba55df53f
Shen Yichen <2007.yichen@gmail.com>
2013-02-02 19:07:50 +0800

    GUI Framework

---
+++ src/SubwayMaker/MapController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SubwayMaker;
+
+import java.awt.Graphics2D;
+
+/**
+ *
+ * @author Yichen
+ */
+public class MapController {
+    
+    public MapController(){
+        
+    }
+    
+    public void paint(Graphics2D g){
+        
+    }
+}
